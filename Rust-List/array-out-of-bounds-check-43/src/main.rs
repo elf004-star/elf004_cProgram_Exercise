e0a8acaa350dcb86440f23a74059785c01929903
elf004 <1873475824@qq.com>
2026-05-10 03:35:41 +0800

    add 4.2~4.4

---
+++ Rust-List/array-out-of-bounds-check-43/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let arr = [1, 2, 3];
+    // let elem = arr[10];  // 运行时 panic 崩溃
+    // 安全访问用 get
+    match arr.get(10) {
+        Some(elem) => println!("{}", elem),
+        None => println!("越界了"),
+    }
+}
