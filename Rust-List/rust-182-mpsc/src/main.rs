f9599d895f4c111727c704fae26e0154fcae6356
elf004-apple <1873475824@qq.com>
2026-07-13 23:07:26 +0800

    add 181~183

---
+++ Rust-List/rust-182-mpsc/src/main.rs
@@ -0,0 +1,13 @@
+use std::sync::mpsc;
+use std::thread;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let val = String::from("Hello");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("收到：{}", received);
+}
