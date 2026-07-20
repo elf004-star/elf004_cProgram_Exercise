5edcbc398d4207bbf9700ca1e8522932f1a11cd5
elf004-apple <1873475824@qq.com>
2026-07-20 22:34:07 +0800

    Add data type lesson and UI elements to exercise 002

---
+++ JavaScript-List/002/js-12-data-type.js
@@ -0,0 +1,24 @@
+// 1. String 字符串必须用引号包起来
+
+let myName = "小明";
+
+// 2. Number 数字直接写
+
+let price = 99.9;
+
+// 3. Boolean 布尔值，只有两个
+
+let isAdult = true;
+let isChild = false;
+
+// 4. Array 数组：一个列表，用方括号括起来，里面可以放任何东西
+let myFruits = ["苹果", "香蕉", "西瓜"];
+console.log(myFruits[0]);
+
+// 5. Object 对象：一个“描述”某种事物的集合，用大括号包起来
+let person = {
+  name: "小红",
+  age: 18,
+  isStudent: true
+};
+console.log(person.name);   // 输出：小红
