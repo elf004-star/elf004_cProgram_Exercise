1e6ea0fe9db25ecd52a4ad786dca3529027b9ea0
elf004-apple <1873475824@qq.com>
2026-08-01 03:29:09 +0800

    Refactor JavaScript learning materials and file structure
    - Consolidate common assets and file organization
    - Relocate exercise files to appropriate directories
    - Add foundational HTML/CSS templates and function examples

---
+++ JavaScript-List/002/js-21-function.js
@@ -0,0 +1,9 @@
+// 1. 声明一个函数（就像定义一种“行为”）
+function sayHello() {
+  console.log("Hello!");
+  console.log("欢迎来到JS世界！");
+}
+
+// 2. 调用（执行）这个函数
+sayHello();   // 输出：Hello! 和 欢迎来到JS世界！
+sayHello();   // 重复调用
